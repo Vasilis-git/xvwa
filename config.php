bda3e0a11930d9e961de9a2ae3ae726a56034111
Vasilis Koulouris <basileskouloures@gmail.com>
2023-12-09 12:29:01 +0200

    fixed issue "Add password protection to this database" (SonarCloud)

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $XVWA_WEBROOT = "";
 $host = "localhost";
 $dbname = 'xvwa';
 $user = "root";
-$pass = "";
+$pass = "nA<.Ye^Hv!3cz~}[";//generated from https://passwordsgenerator.net/
 $conn = new mysqli($host,$user,$pass,$dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
